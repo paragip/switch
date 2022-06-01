4981b074b406128cd8c2574f0a950dc371dacd78
paragip <pal.paragi@gmail.com>
2022-06-01 13:06:03 +0200

    switch gyakorlás

---
+++ src/switchPractice.java
@@ -0,0 +1,20 @@
+public class switchPractice {
+    public static void main(String[] args) {
+
+        int switchValue = 4;
+        switch(switchValue) {
+            case 1:
+                System.out.println("Value is 1");
+                break;
+
+            case 2:
+                System.out.println("Value is 2");
+                break;
+
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+        }
+
+    }
+}
