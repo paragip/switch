32510215b6588a25a98e6f5347c544a736a33b45
paragip <pal.paragi@gmail.com>
2022-06-01 14:27:07 +0200

    int helyett char

--- src/switchPractice.java
+++ src/switchPractice.java
@@ -1,7 +1,7 @@
 public class switchPractice {
     public static void main(String[] args) {
 
-        int switchValue = 4;
+ /*       int switchValue = 2;
         switch(switchValue) {
             case 1:
                 System.out.println("Value is 1");
@@ -11,10 +11,37 @@ public class switchPractice {
                 System.out.println("Value is 2");
                 break;
 
+            case 3: case 4: case 5:
+                System.out.println("Value is 3 or 4 or 5");
+                break;
+
             default:
-                System.out.println("Was not 1 or 2");
+                System.out.println("Was not 1-5");
+                break;
+        } */
+
+        char letter = 'B';
+        switch(letter) {
+            case 'A':
+                System.out.println("Letter is A");
+                break;
+            case 'B':
+                System.out.println("Letter is B");
+                break;
+            case 'C':
+                System.out.println("Letter is C");
+                break;
+            case 'D':
+                System.out.println("Letter is D");
+                break;
+            case 'E':
+                System.out.println("Letter is E");
+                break;
+            default:
+                System.out.println("None of A or B or C or D or E");
                 break;
         }
+
 
     }
 }
